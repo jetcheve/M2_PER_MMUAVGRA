b863d998c509bf427cf7f7a971151008ab846cbf
mika33666 <mickael.testa@gmail.com>
2013-12-09 11:20:51 +0100

    ajout Scannode et scan de la map

--- dev/src/random_mobility/MovingNode.java
+++ dev/src/random_mobility/MovingNode.java
@@ -1,23 +1,37 @@
 package random_mobility;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import jbotsim.Clock;
 import jbotsim.Node;
+import jbotsim.Topology;
 import jbotsim.event.ClockListener;
 
 public class MovingNode extends Node implements ClockListener{
 	private int lastdirection; //  1 -> gauche , 2-> tout droit, 3 -> droite
 	private double angle_direction; //direction d'un UAV en Radian.
 	private static double amplitude_variation_direction = Math.PI/4; // l'amplitude du changement de direction
-
-	public MovingNode(){
+//	private Function_mobility_model func;
+	private HashMap<Integer, ArrayList<Integer>> map;
+	private static double totalscanpossible = 500 * 500;
+	private static int time;
+	
+	public MovingNode(/*Function_mobility_model fun*/){
 		setProperty("icon", "/avion.png");
         setProperty("size", 20);
-		Clock.addClockListener(this, 1);
+        setCommunicationRange(-1);
+        Clock.addClockListener(this, 1);
 		angle_direction = 3*Math.PI/2;  //direction : vers le haut.
 		setDirection(angle_direction);
 		lastdirection = 2;
+		time = 0;
+		map = new HashMap<Integer, ArrayList<Integer>>();
+		//func = fun;
 	}
 
 	public void onClock(){
+		time++;
 		int alea = (int) (Math.random()*10);
 		switch (lastdirection){
 
@@ -69,5 +83,32 @@ public class MovingNode extends Node implements ClockListener{
 		}      
 		move(1);
 		wrapLocation();
+		Point2D pos = getLocation();
+		int x = (int)pos.getX();
+		int y = (int)pos.getY();
+		scan((int)pos.getX(), (int)pos.getY());
+		System.out.println(Main.totalscan);
+		afficher_pourcentage_scan();
+		afficher_heure();
+	}
+	
+	private void afficher_heure() {
+		System.out.println("Le test a commencé depuis " +time + "toc d'horloge");
+	}
+
+	public void scan(int x, int y){
+		if(Main.map[x][y]==0)
+		{
+			Main.map[x][y]=1;
+			Main.totalscan++;
+			getTopology().addNode(x, y, new ScanNode());
+		}
+		else
+			System.out.println("DEJA SCANNNER");
+	}
+	
+	public void afficher_pourcentage_scan()
+	{
+		System.out.println("Scan : "+ (Main.totalscan/totalscanpossible*100) + "%");
 	}
 }
--- dev/src/random_mobility/main.java
+++ dev/src/random_mobility/main.java
@@ -3,10 +3,20 @@ import jbotsim.ui.JViewer;
 import jbotsim.Topology;
 import jbotsim.Node;
 
-public class main{
-    public static void main(String[] args)
-    {
-	Node.setModel("default", new MovingNode());
-	new JViewer(new Topology());
-    }
+public class Main{
+
+	private static int dimension = 500;
+	public  static int[][] map = new int[500][500];
+	public static double totalscan = 0;
+
+	public static void main(String[] args)
+	{
+		Topology topo = new Topology();
+		topo.setDimensions(dimension, dimension);
+		Node.setModel("default", new MovingNode());
+		Node.setModel("scan", new ScanNode());
+		JViewer jv = new JViewer(topo);
+		for(int i= 0;i<10;i++)
+			topo.addNode(dimension/2, dimension);
+	}
 }
